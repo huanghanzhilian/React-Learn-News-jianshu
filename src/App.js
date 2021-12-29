68610f2cf109df3c5b8710ea4cf8b1498a391a0d
huangjipeng <1319639755@qq.com>
2021-12-29 15:19:43 +0800

    Waht is component?

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div>Hello World!</div>
     )
   }
 }
 
-/*function App() {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
-}*/
 
 export default App;
