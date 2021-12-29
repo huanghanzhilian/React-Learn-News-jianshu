9fc241eabb7a21f3f5c1344f8bf6ba17f3ac3394
huangjipeng <1319639755@qq.com>
2021-12-29 16:42:18 +0800

    reactive design ideas and event binding in react.

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,31 @@
 import React, { Component, Fragment } from 'react'
 
 class TodoList extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      inputValue: 'hello',
+      list: []
+    }
+  }
+
+  handleInput (el) {
+    this.setState({
+      inputValue: el.target.value
+    })
+  }
+
   render () {
     return (
       <Fragment>
-        <div><input /><button>submit</button></div>
+        <div>
+          <input
+            value={ this.state.inputValue }
+            onInput={ this.handleInput.bind(this) }
+          />
+          <button>submit</button>
+        </div>
         <ul>
           <li>learning english.</li>
           <li>learning react.</li>
