9b0b1e210f9deacaba11a83c17a5adf6c8a8edc5
huangjipeng <1319639755@qq.com>
2021-12-29 15:43:48 +0800

    What is JSX?

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 class App extends Component {
   render () {
+    // JSX
     return (
       <div>Hello World!</div>
     )
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom'
 
 import App from './App'
 
+// JSX 语法中
+// 我们可以自定义标签，也就是自定义组件
+// 如果我们要适应自己创建的组件 组件开头必须是大写
+// 小写的是h5原生标签
 ReactDOM.render(
   <React.StrictMode>
     <App />
