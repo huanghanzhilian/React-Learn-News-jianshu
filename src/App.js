b4ef38314a9f117ae64162b5e17375d21d41b2ed
huangjipeng <1319639755@qq.com>
2022-01-04 15:55:35 +0800

    react transition group 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component, Fragment } from 'react'
 
+import { CSSTransition } from 'react-transition-group'
+
 import './style.css'
 
 class App extends Component {
@@ -23,7 +25,16 @@ class App extends Component {
   render () {
     return (
       <Fragment>
-        <div className={ this.state.show ? 'show' : 'hide' }>hello</div>
+        <CSSTransition
+          in={ this.state.show }
+          timeout={1000}
+          classNames="fade"
+          unmountOnExit
+          onEntered={(el) => { el.style.color = 'blue' }}
+          appear={true}
+        >
+          <div>hello</div>
+        </CSSTransition>
         <button onClick={ this.handleToggle }>toggle</button>
       </Fragment>
     )
