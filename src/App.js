08084116850be858efc0d5e7b49dd01695506e98
huangjipeng <1319639755@qq.com>
2022-01-04 16:17:59 +0800

    react transition group use q

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react'
 
-import { CSSTransition } from 'react-transition-group'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 import './style.css'
 
@@ -10,32 +10,43 @@ class App extends Component {
     super(props)
 
     this.state = {
-      show: true
+      list: ['item']
     }
 
-    this.handleToggle = this.handleToggle.bind(this)
+    this.handleAddItem = this.handleAddItem.bind(this)
   }
 
-  handleToggle () {
-    this.setState({
-      show: this.state.show ? false : true
+  handleAddItem () {
+    this.setState((prevState) => {
+      return {
+        list: [...prevState.list, 'item']
+      }
     })
   }
 
   render () {
     return (
       <Fragment>
-        <CSSTransition
-          in={ this.state.show }
-          timeout={1000}
-          classNames="fade"
-          unmountOnExit
-          onEntered={(el) => { el.style.color = 'blue' }}
-          appear={true}
-        >
-          <div>hello</div>
-        </CSSTransition>
-        <button onClick={ this.handleToggle }>toggle</button>
+        <TransitionGroup className="todo-list">
+          {
+            this.state.list.map((item, index) => {
+              return (
+                <CSSTransition
+                  in={ this.state.show }
+                  timeout={1000}
+                  classNames="fade"
+                  unmountOnExit
+                  onEntered={(el) => { el.style.color = 'blue' }}
+                  appear={true}
+                  key={index}
+                >
+                  <div>{ item }</div>
+                </CSSTransition>
+              )
+            })
+          }
+        </TransitionGroup>
+        <button onClick={ this.handleAddItem }>add item</button>
       </Fragment>
     )
   }
