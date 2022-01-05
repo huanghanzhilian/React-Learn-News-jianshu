2b189036ff475681b0ffc0189b085b1ad5e506e3
huangjipeng <1319639755@qq.com>
2022-01-05 13:37:34 +0800

    redux actionTypes break down.

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,6 +4,10 @@ import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd';
 
 import store from './store'
+import {
+  CHANGE_INPUT_VALUE,
+  ADD_TODO_ITEM,
+  DELETE_TODO_ITEM } from './store/actionTypes'
 
 class TodoList extends Component {
 
@@ -61,7 +65,7 @@ class TodoList extends Component {
 
   handleInputValue (e) {
     const action = {
-      type: 'change_input_value',
+      type: CHANGE_INPUT_VALUE,
       value: e.target.value
     }
     store.dispatch(action)
@@ -73,14 +77,14 @@ class TodoList extends Component {
 
   handleBtnClick () {
     const action = {
-      type: 'add_todo_item'
+      type: ADD_TODO_ITEM
     }
     store.dispatch(action)
   }
 
   handleItemDelete (index) {
     const action = {
-      type: 'delete_todo_item',
+      type: DELETE_TODO_ITEM,
       index
     }
     store.dispatch(action)
