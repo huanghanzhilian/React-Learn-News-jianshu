6a303b5d5192bd4dd7e6b2606ef1829c1f0199ad
huangjipeng <1319639755@qq.com>
2022-01-06 23:39:22 +0800

    use styled-component develop header.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 
+import Header from './common/Header'
+
 class App extends Component {
   render () {
     return (
       <div>
-        hello world
+        <Header />
       </div>
     )
   }
