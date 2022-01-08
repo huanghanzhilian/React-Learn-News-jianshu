ff6dac57c43ab3be0ee4510cddfabf3ea6ee30a1
huangjipeng <1319639755@qq.com>
2022-01-08 23:41:05 +0800

    immutable first use

--- src/common/Header/store/reducer.js
+++ src/common/Header/store/reducer.js
@@ -1,19 +1,20 @@
+import { fromJS } from 'immutable'
+// immutable 库
+// immutable 对象
 import * as constants from './constants'
 
-const defaultState = {
+const defaultState = fromJS({
   focused: false
-}
+})
 
 export default (state = defaultState, action) => {
   if (action.type === constants.SEARCH_FOCUS) {
-    const newState = JSON.parse(JSON.stringify(state))
-    newState.focused = true
-    return newState
+    // immutable对象的set方法，会结合之前的immutable对象的值
+    // 和设置的值，返回一个全新的的对象
+    return state.set('focused', true)
   }
   if (action.type === constants.SEARCH_BLUR) {
-    const newState = JSON.parse(JSON.stringify(state))
-    newState.focused = false
-    return newState
+    return state.set('focused', false)
   }
   return state
 }
