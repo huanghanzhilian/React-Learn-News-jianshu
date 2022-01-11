45a5b5ad72344abaaa7c2233b7681c40a0178eaa
huangjipeng <1319639755@qq.com>
2022-01-11 12:54:10 +0800

    home page data from axios request.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 
+import { connect } from 'react-redux'
+import axios from 'axios'
+
 import Topic from './components/Topic'
 import Recommend from './components/Recommend'
 import List from './components/List'
@@ -29,6 +32,31 @@ class HomePage extends Component {
       </HomeWrapper>
     )
   }
+
+  componentDidMount () {
+    axios.get('/api/home.json').then(res => {
+      const result = res.data.data
+      const action = {
+        type: 'change_home_data',
+        topicList: result.topicList,
+        articleList: result.articleList,
+        recommendList: result.recommendList,
+        writerList: result.writerList
+      }
+      this.props.changeHomeData(action)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
 }
 
-export default HomePage
+const mapState = (state) => ({})
+
+const mapDispatch = (dispatch) => ({
+  changeHomeData(action) {
+    dispatch(action)
+  }
+})
+
+
+export default connect(mapState, mapDispatch)(HomePage)
